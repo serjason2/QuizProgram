2406396aac6956654beaf4664ecbe6168ad06451
Jason S <serjason2@gmail.com>
2024-09-26 15:19:17 -0500

    Renamed file to QuizProgram

---
+++ QuizProgram.java
@@ -0,0 +1,10 @@
+// Quiz Program
+// 4 questions, 4 multiple choice answers. Questions varies related to (fun facts, programming, sports, and video games)
+
+import java.util.Scanner; // for user input
+import java.util.HashMap; // to store questions and answers
+
+public class QuizProgram {
+    int score = 0;
+
+} // Main class
